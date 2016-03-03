def8a23d248268a7be1abc4f5b2e88701fca8c8f
Peilin Li <lipeilin@gatech.edu>
2016-03-02 23:04:50 -0500

    merged

--- js/PlayerData.js
+++ js/PlayerData.js
@@ -1,3 +1,8 @@
+angular.module("AntiAddicter").controller("playerData",['$scope',"userInfo", function($scope,userInfo) {
+	
+
+
+
 var https = require('https');
 var fs = require("fs");
 
@@ -237,7 +242,7 @@ var AnalyzeMatchData = function(matchStrArray){
 //24229424
 
 //Inputs
-var summoner_name_original = "This is Why";
+var summoner_name_original = userInfo.username;
 var summoner_name_original_escaped = encodeURI(summoner_name_original);
 
 var apiKey = ["79cfb0e6-89a2-4a0b-95c0-77238c9c6afe", "eb44fe5e-8a30-4eaa-8376-69d39f8c6832"];
@@ -254,5 +259,5 @@ getSummonerIdByName(summoner_name, getSummonerMatchList, GetMatchData);
 
 
 
-
+}])
 
