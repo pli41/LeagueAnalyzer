900946427816e1c843f74c2406d5a86dc38b5513
Peilin Li <lipeilin@gatech.edu>
2016-03-07 15:54:09 -0500

    analysis done

--- js/PlayerDataTest.js
+++ js/PlayerDataTest.js
@@ -1,8 +1,9 @@
 
-
-		
-	//var https = require('https');
-	//var fs = require("fs");
+	
+	var summoner_name_original = 'Bo xilai';
+	var apiKey = ["79cfb0e6-89a2-4a0b-95c0-77238c9c6afe", "eb44fe5e-8a30-4eaa-8376-69d39f8c6832"];
+	
+	//deff4e7a-16c6-4144-9d31-0305f9b3d19f
 
 	var getSummonerIdByName = function(name, callback1, callback2){
 		var summoner_name_escaped= encodeURI(name);
@@ -195,12 +196,119 @@
 		
 		var analysis = '';
 		
+		
+		
+		
+		
 		//KDA
 		var totalKills = 0;
 		var totalDeaths = 0;
 		var totalAssists = 0;
+		var gatherKDA_Data = function(match, participantID){
+			console.log(`KDA participant ID is ${participantID}`);
+			totalKills += match.participants[participantID-1].stats.kills;
+			totalDeaths += match.participants[participantID-1].stats.deaths;
+			totalAssists += match.participants[participantID-1].stats.assists;
+		}
+	
+	
+		//vision control
+		var wardingValue = 0;
+		var gatherVisionControlData = function(match, participantID){
+			//wardingValue += match.participants[participantID-1].stats.wardsPlaced;
+			//wardingValue += match.participants[participantID-1].stats.wardsKilled * 2;
+			wardingValue += match.participants[participantID-1].stats.visionWardsBoughtInGame;
+		}
 		
 		
+		//target control
+		var dragonSlained = 0;
+		var riftHeraldSlained = 0;
+		var baronSlained = 0;
+		var gatherTargetControlData = function(match, participantID){
+			var participant_teamID = match.participants[participantID-1].teamId;
+			for(var i = 0; i < 2; i ++){
+				if(match.teams[i].teamId === participant_teamID){
+					dragonSlained += match.teams[i].dragonKills;
+					riftHeraldSlained += match.teams[i].riftHeraldKills;
+					baronSlained += match.teams[i].baronKills;
+				}
+			}
+		}
+		
+		//Kill contribution
+		var totalKills_AllMatches = 0;
+		var totalKills_KillContribution = 0;
+		var totalAssists_KillContribution = 0;
+		var gatherKillContribution = function(match, participantID){
+			var participant_teamID = match.participants[participantID-1].teamId;
+			
+			for(var i = 0; i < 10; i++){
+				if(match.participants[i].teamId === participant_teamID){
+					totalKills_AllMatches += match.participants[i].stats.kills;
+				}
+			}
+			
+			totalKills_KillContribution += match.participants[participantID-1].stats.kills;
+			totalAssists_KillContribution += match.participants[participantID-1].stats.assists;
+		}
+		
+		//match duration
+		var match_totalDuration = 0;
+		var gatherMatchDuration = function(match){
+			match_totalDuration += match.matchDuration;
+		}
+		
+		//win rate
+		var match_wins = 0;
+		var gatherMatchWinRate = function(match, participantID){
+			if(match.participants[participantID-1].stats.winner){
+				match_wins++;
+			}
+		}
+		
+		//Versatility
+		var topPlayed = 0;
+		var midPlayed = 0;
+		var junglePlayed = 0;
+		var adcPlayed = 0;
+		var supportPlayed = 0;
+		var gatherVersatilityData = function(match, participantID){
+			if(match.participants[participantID-1].timeline.lane === 'TOP'){
+				topPlayed ++;
+			}
+			else if(match.participants[participantID-1].timeline.lane === 'MIDDLE'){
+				midPlayed ++;
+			}
+			else if(match.participants[participantID-1].timeline.lane === 'JUNGLE'){
+				junglePlayed ++;
+			}
+			else if(match.participants[participantID-1].timeline.lane === 'BOTTOM'){
+				if(match.participants[participantID-1].timeline.role === 'DUO_SUPPORT'){
+					supportPlayed ++;
+				}
+				else{
+					adcPlayed ++;
+				}
+			}
+		}
+		
+		
+		//Tower Kills
+		var total_Towerkills = 0;
+		var gatherTowerKills = function(match, participantID){
+			total_Towerkills += match.participants[participantID-1].stats.towerKills;
+		}
+		
+		
+		//crowd control time
+		var control_Duration = 0;
+		var gatherControlDuration = function(match, participantID){
+			control_Duration += match.participants[participantID-1].stats.totalTimeCrowdControlDealt;
+		}
+		
+		
+		//Start analyzing
 		for (var i = 0; i < matchJson.matches.length; i++){
 			
 			var match = matchJson.matches[i];
@@ -211,20 +319,38 @@
 			for (var j = 0; j < match.participantIdentities.length; j++){
 				identity = match.participantIdentities[j];
 				//console.log(`current player name: ${identity.player.summonerName}`);
-				if(identity.player.summonerName == summoner_name_original){
+				if(identity.player.summonerName.toLowerCase() === summoner_name_original.toLowerCase()){
 					participantID = identity.participantId;
 				}
 			};
 			
 			//KDA
-			totalKills += match.participants[participantID-1].stats.kills;
-			totalDeaths += match.participants[participantID-1].stats.deaths;
-			totalAssists += match.participants[participantID-1].stats.assists;
-
+			gatherKDA_Data(match, participantID);
+			gatherVisionControlData(match, participantID);
+			gatherKillContribution(match, participantID);
+			gatherMatchDuration(match);
+			gatherTargetControlData(match, participantID);
+			gatherMatchWinRate(match, participantID);
+			gatherVersatilityData(match, participantID);
+			gatherTowerKills(match, participantID);
+			gatherControlDuration(match, participantID)
 		};
 		
 		//KDA
 		var KDA = (totalKills + totalAssists) / Math.max(1, totalDeaths);
+		
+		
+		//KillContribution
+		var KillContribution = Math.round((totalKills_KillContribution+totalAssists_KillContribution)/totalKills_AllMatches*100);
+		
+		//Average game time
+		var avgDuration = match_totalDuration/matchJson.matches.length;
+		var avgDuration_parsed = (Math.floor(avgDuration/60)).toString() + 'min' + (Math.floor(avgDuration%60)) + 'sec';
+		
+		//Win Rate
+		var winRate = Math.round(match_wins/matchJson.matches.length*100);
+		
+		var control_Duration_parsed = (Math.floor(control_Duration/60)).toString() + 'min' + (Math.floor(control_Duration%60)) + 'sec';
 		
 		//format data
 		analysis += `Total Kills: ${totalKills}`;
@@ -234,18 +360,47 @@
 		analysis += `Total Assists: ${totalAssists}`;
 		analysis += '\n';
 		analysis += `KDA: ${KDA}`;
-		fs.writeFileSync('Analysis.txt', analysis);
+		analysis += '\n';
+		analysis += `Vision Control: ${wardingValue}`;
+		//fs.writeFileSync('Analysis.txt', analysis);
+		
 		console.log('analysis file generated');
+		console.log('\n');
+		console.log(`	Analysis of ${summoner_name_original}`);
+		console.log('\n');
+		console.log(`		Data from the recent ${matchJson.matches.length} matches`);
+		console.log('\n');
+		console.log(`		KDA: ${KDA}`);
+		console.log(`			Average Kills: ${totalKills/10}`);
+		console.log(`			Average Deaths: ${totalDeaths/10}`);
+		console.log(`			Average Assists: ${totalAssists/10}`);
+		console.log(`		Positions Played:`);
+		console.log(`			TOP ${topPlayed}`);
+		console.log(`			JUNGLE ${junglePlayed}`);
+		console.log(`			MID ${midPlayed}`);
+		console.log(`			ADC ${adcPlayed}`);
+		console.log(`			SUPPORT ${supportPlayed}`);
+		console.log(`		Win Rate: ${winRate}%`);
+		console.log(`		Vision Control: ${wardingValue} vision wards bought`);
+		console.log(`		Kill Contribution: ${KillContribution}%`);
+		console.log(`		Average match length: ${avgDuration_parsed}`);
+		console.log(`		Target Control: ${total_Towerkills} towers/${dragonSlained} dragons/${riftHeraldSlained} rift Heralds/${baronSlained} barons`);
+		console.log(`		Total CC duration: ${control_Duration_parsed}`);
+		
+		total_Towerkills
+		control_Duration
 	}
 
+	
 
 	//Inputs
 	var https = require('https');
 	var fs = require('fs');
 	
-	var summoner_name_original = 'xPLzzZx';
-	var apiKey = ["79cfb0e6-89a2-4a0b-95c0-77238c9c6afe", "eb44fe5e-8a30-4eaa-8376-69d39f8c6832"];
 
+
+	
+	
 	var summoner_name = summoner_name_original.toLowerCase();
 
 	getSummonerIdByName(summoner_name, getSummonerMatchList, GetMatchData);
