8362f8d140ced6db6fcb344b446e562a5857b586
Peilin Li <lipeilin@gatech.edu>
2016-04-06 18:10:31 -0400

    path fixed

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ requirejs.config({
         'angular': 'https://ajax.googleapis.com/ajax/libs/angularjs/1.4.9/angular.min',
         'angular-route': 'https://ajax.googleapis.com/ajax/libs/angularjs/1.2.5/angular-route.min',
         'bootstrap': 'http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.1/js/bootstrap.min',
-		'chart': '../node_modules/Chart.js/Chart.min',
+		'chart': '../node_modules/chart.js/Chart.min',
 		'tc.chartjs': '../node_modules/tc-angular-chartjs/dist/tc-angular-chartjs.min',
     },
     shim: {
